cbcd57b9d41900de35ff6d17623c9b85afbddc0d
ReneNeubert <rene.neubert@softcake.og>
2017-02-03 15:19:00 +0100

    feat: readme

--- documentation/src/test/java/example/DocExampleTest.java
+++ documentation/src/test/java/example/DocExampleTest.java
@@ -16,7 +16,7 @@
 
 package example;
 
-import org.softcake.one.One;
+import com.google.common.base.Preconditions;
 
 /**
  * @author René Neubert.
@@ -26,11 +26,11 @@ public final class DocExampleTest {
     private DocExampleTest() {
 
     }
+
     // tag::example[]
     public static void main(final String[] args) {
 
-        One one = new One("Hello World");
-        System.out.println(one.getName());
+        Preconditions.checkNotNull("Hello");
 
     }
     // end::example[]
